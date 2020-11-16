d11bd511943489dc75ad142af09fae5ce6ff4854
Miguel  Arreola <marreola4619@gmail.com>
2020-11-16 22:40:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,4 @@
-# FIRST METHOD THAT PASSED
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
 
-
-# FINAL SOLUTION
-def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
-end
 
 
 
